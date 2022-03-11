d6afc7c7b435244965f1142bdac455187eb544f2
xiaolaba <xiao_laba_cn@yahoo.com>
2022-03-11 09:31:57 +0800

    Update Program.cs

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -14,7 +14,9 @@ namespace Sample
             oauth2.sync("yourmail@gmail.com", "client path (json)");
 
             Console.WriteLine(String.Format("Email: {0}\nAccessToken: {1}\nRefreshToken: {2}\nClient_Id: {3}\nClient_Secret: {4}", oauth2_account.mail_address, oauth2_account.oauth2_access_Token, oauth2_account.oauth2_refresh_Token, oauth2_account.client_id, oauth2_account.client_secret));
-            oauth2.send_Gmail(new string[] { "to1@gmail.com", "to2@yahoo.co.jp" }, "test", "Heloooooooo", "gmail_oauth2", "path/image.png", "iamge", "png");
+            //oauth2.send_Gmail(new string[] { "to1@gmail.com", "to2@yahoo.co.jp" }, "test", "Heloooooooo", "gmail_oauth2", "path/image.png", "iamge", "png");
+            oauth2.send_Gmail(new string[] { "to1@gmail.com", "to2@yahoo.co.jp" }, "subject_test標題繁體简体", "Heloooooooo 繁體简体", "gmail_oauth2", "path/image.png", "iamge", "png");
+            Console.WriteLine(String.Format("\ndone\npress any key to exit"));
             Console.ReadKey();
         }
     }
